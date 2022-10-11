d74a112a4bf402351a02721b69f70e040ff60f63
Darryl <darrylo2020@gmail.com>
2022-10-11 12:22:29 -0400

    created a function that when called should add the move and then there should be another function that creates another board. right?

--- src/project1/SuperTicTacToe.java
+++ src/project1/SuperTicTacToe.java
@@ -12,4 +12,10 @@ public class SuperTicTacToe {
         game = new SuperTicTacToeGame();
         game_panel = new SuperTicTacToePanel(game);
     }
+
+    public SuperTicTacToe(SuperTicTacToeGame move){
+        moves_history.add(move); // everytime a move is made call this function and add the board first than create the new board.
+    }
+
+
 }
