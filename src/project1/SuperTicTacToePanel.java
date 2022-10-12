da47a2971b0bb13fd1df449325a3f222cc919754
Redemxx <lost_diamond@charter.net>
2022-10-12 12:40:13 -0400

    Moved SuperTicTacToePanel's main code into the instantiate function. Updated getSizeInput function that takes in the board size from the user at runtime.

--- src/project1/SuperTicTacToePanel.java
+++ src/project1/SuperTicTacToePanel.java
@@ -22,40 +22,22 @@ public class SuperTicTacToePanel extends JPanel {
     private JMenuItem undoItem; //Create keyboard shortcut?
     private JMenuItem redoItem; //Create keyboard shortcut?
 
-    public void main(String[] args) {
-
-        fileMenu = new JMenu("File");
-        quitItem = new JMenuItem("Quit!");
-        undoItem = new JMenuItem("Undo!");
-        redoItem = new JMenuItem("Redo!");
-
-        fileMenu.add(quitItem);
-        fileMenu.add(undoItem);
-        fileMenu.add(redoItem);
-
-        quitItem.addActionListener(new MenuHandler());
-        undoItem.addActionListener(new MenuHandler()::actionPerformedB);
-        redoItem.addActionListener(new MenuHandler()::actionPerformedC);
-
-        menuBar.add(fileMenu);
-        //OUR-GUI-NAME.add(menuBar);
-    }
-
     public SuperTicTacToeGame get_game() {
         return this.game;
     }
     public SuperTicTacToePanel() {
-        String s = getInput("Enter desired size of the TicTacToe board:");
-        game = new SuperTicTacToeGame(Integer.parseInt(s));
+        game = new SuperTicTacToeGame(getSizeInput("Enter desired size of the TicTacToe board:"));
+        instantiateInstance();
     }
 
     public SuperTicTacToePanel(SuperTicTacToeGame game) {
         this.game = game;
+        instantiateInstance();
     }
 
     private void instantiateInstance() {
-        xIcon = new ImageIcon("x.png");
-        oIcon = new ImageIcon("o.jpg");
+//        xIcon = new ImageIcon("x.png");
+//        oIcon = new ImageIcon("o.jpg");
         emptyIcon = new ImageIcon();
 
         int s = game.getDimension();
@@ -70,10 +52,38 @@ public class SuperTicTacToePanel extends JPanel {
 
         gui = new JFrame("TicTacToe");
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        fileMenu = new JMenu("File");
+        quitItem = new JMenuItem("Quit!");
+        undoItem = new JMenuItem("Undo!");
+        redoItem = new JMenuItem("Redo!");
+
+        fileMenu.add(quitItem);
+        fileMenu.add(undoItem);
+        fileMenu.add(redoItem);
+
+        quitItem.addActionListener(new MenuHandler());
+        undoItem.addActionListener(new MenuHandler()::actionPerformedB);
+        redoItem.addActionListener(new MenuHandler()::actionPerformedC);
+        JMenuBar menus = new JMenuBar();
+        menus.add(fileMenu);
+
+        gui.setSize(1100,480);
+        gui.setJMenuBar(menus);
+        gui.setVisible(true);
     }
 
-    private String getInput(String message) {
-        return (String)JOptionPane.showInputDialog(message);
+    private int getSizeInput(String message) {
+        int input_size = 0;
+        try {
+            while (input_size <= 2 || input_size >= 15  ) {
+                String s = (String) JOptionPane.showInputDialog(null, message);
+                input_size = Integer.parseInt(s);
+            }
+        }catch (Exception e) {
+            input_size = 3;
+        }
+        return input_size;
     }
 
     private class ButtonListener implements ActionListener {
